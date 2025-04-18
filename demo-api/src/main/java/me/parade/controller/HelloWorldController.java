1ab0060748629880385ca6693f53f02b4bb02e44
flb <13703937803@163.com>
2025-04-18 16:31:42 +0800

    refactor: 修改包名

--- demo-api/src/main/java/me/parade/controller/HelloWorldController.java
+++ demo-api/src/main/java/me/parade/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.example.controller;
+package me.parade.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
